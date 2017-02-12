51aeba1eaf4e3b1ec571b85006504210a2dc8bf1
irmantasaudejas <iirmantass@gmail.com>
2017-02-12 23:13:45 +0200

    Added CSS to content.php

--- content.php
+++ content.php
@@ -4,6 +4,7 @@
     <title>Knygos aprašymas</title>
 </head>
 <body>
+    <link href="css/main.css" rel="stylesheet" type="text/css" >
     <?php
         require('/includes/db_conn.php'); //includinami DB duomenys
         $conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name); //jungiamasi prie db
@@ -16,7 +17,7 @@
         $query = "SELECT * FROM `books` WHERE `book_id` = ".$bookID  ; //vykdoma uzklausa kurioje isrenkama pasirinktos knygos duomenys
         $result = mysqli_query($conn,$query);
         $data = mysqli_fetch_assoc($result); //gaunami knygos duomenys
-        echo "Knygos autorius: " . $data['book_author'] . "<br />" . "Knygos pavadinimas: " .$data['book_name'] . "<br />" . "Išleidimo metai: " . $data['publishYear'] . "<br />" . " Knygos žanras: " . $data['book_genre'] . "<br />"  ; //atvaizduojami
+        echo "<p>Knygos autorius: " . $data['book_author'] . "<br />" . "Knygos pavadinimas: " .$data['book_name'] . "<br />" . "Išleidimo metai: " . $data['publishYear'] . "<br />" . " Knygos žanras: " . $data['book_genre'] . "</p><br />"  ; //atvaizduojami
         echo "<a href= 'index.php' class='book' > Atgal </a>"; // formuojamas atgal mygtukas
         mysqli_close($conn); //nutraukiamas su DB rysis
     ?>
